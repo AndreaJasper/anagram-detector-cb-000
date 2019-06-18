6f25e95d2faf6f8f0807dfb2e55899b6a1bd8456
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-06-18 14:53:46 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,13 +1,15 @@
 # Your code goes here!
 class Anagram
   @anagram = anagram
-  @@all = all
+  @@all = []
 
   def initialize(word)
     @word = word
   end
 
   def word.match
+    array = []
+    if
     #should return all possible matches
     #return empty arrap if no match exists
   end
