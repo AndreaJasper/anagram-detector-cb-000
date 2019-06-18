b043d64504fcde39a5ea36f166ab05b196ca8e16
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-06-18 14:51:20 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+  @anagram = anagram
+  @@all = all
+
+  def initialize(word)
+    @word = word
+  end
+
+  def word.match
+    #should return all possible matches
+    #return empty arrap if no match exists
+  end
+end
